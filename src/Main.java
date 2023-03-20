f3617cb3418ab805b76832705038949b4732ebe0
IvanMR94 <106714183+IvanMR94@users.noreply.github.com>
2023-03-20 11:16:10 -0300

    Update Main.java
    Eliminación de comentarios obsoletos de la clase Main.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        //equipo equipo1 = new equipo("belgrano", 1);
-        //equipo equipo2 = new equipo("talleres", 0);
-
-        //System.out.println("| Equipos Elistados: " +  equipo1 + "  ||  " + equipo2 + " | ");
-
         equipo arr[] = { new equipo("Belgrano", 3), new equipo("talleres", 1) };
 
         pronostico.crearPronostico(arr);
